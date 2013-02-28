35cdddd4a6e9ad4089a1f828bd135e2e5989f3a5
Oleksandr Kovalov <oleksandrk@laptop1211.local>
2013-02-28 13:35:44 -0500

    Refactored background.js to follow jsLint coding standards and did some JS refactoring

--- background.js
+++ background.js
@@ -1,84 +1,101 @@
-var authTabId = null;
+/*global chrome, alert */
 
-function getClickHandler()
-{
-  return function(info, tab)
-  {
-    imgSrc = info.srcUrl;
-    chrome.storage.local.get({'vkaccess_token': {}}, function(items)
-    {
-      if(items['vkaccess_token'].length > 0)
-      {
-        var imgUrl = 'upload.html#' + imgSrc + '&' + items['vkaccess_token'];
-        chrome.tabs.create({url: imgUrl,selected: true});
-      }
-      else
-      {
-        var authUrl = 'https://oauth.vk.com/authorize?client_id=3315996&scope=docs,offline&redirect_uri=http%3A%2F%2Foauth.vk.com%2Fblank.html&display=page&response_type=token';
-        chrome.tabs.create({url: authUrl,selected: true}, function(tab)
-        {
-          authTabId = tab.id;
-          chrome.tabs.onUpdated.addListener(function tabUpdateListener(tabId, changeInfo)
-          {
-            if(tabId == authTabId && changeInfo.url != undefined && changeInfo.status == "loading")
-            {
-              if ( changeInfo.url.indexOf('oauth.vk.com/blank.html') > -1 )
-              {
-                authTabId = null;
-                chrome.tabs.onUpdated.removeListener(tabUpdateListener);
-                var accToken = getUrlParam(changeInfo.url, 'access_token');
-                if( accToken != undefined && accToken.length > 0)
-                {
-                  if(Number(getUrlParam(changeInfo.url, 'expires_in') == 0))
-                  {
-                    chrome.storage.local.set({'vkaccess_token': accToken}, function()
-                    {
-                      chrome.tabs.update(tabId,{'url': 'upload.html#' + imgSrc + '&' + accToken,'active': true}, function(tab){});
-                    });
-                  }
-                  else
-                  {
-                    thereIsAnError('vk auth response problem', 'expiresIn != 0');
-                  }
-                }
-                else
-                {
-                  thereIsAnError('vk auth response problem', 'access_token length = 0 or accToken == undefined');
-                }
-              }
-            }
-          });
-        });
-      }
-    });
-  };
-};
+function thereIsAnError(textToShow, errorToShow) {
+    "use strict";
 
-function getUrlParam(url, sname)
-{
-  var params = url.substr(url.indexOf("#") + 1);
-  var sval = "";
-  params = params.split("&");
-  for(var i = 0; i < params.length; i++)
-  {
-    temp = params[i].split("=");
-    if([temp[0]] == sname)
-    {
-      sval = temp[1];
-    }
-  }
-  return sval;
+    alert(textToShow + '\n' + errorToShow);
 }
 
-chrome.contextMenus.create(
-{
-  "title": "Rehost on vk.com",
-  "type": "normal",
-  "contexts": ["image"],
-  "onclick": getClickHandler()
+function getUrlParam(url, sname) {
+    "use strict";
+
+    var params = url.substr(url.indexOf("#") + 1),
+        sval   = "",
+        i,
+        temp;
+
+    params = params.split("&");
+
+    for (i = 0; i < params.length; i += 1) {
+        temp = params[i].split("=");
+
+        if (temp[0] === sname) {
+            sval = temp[1];
+        }
+    }
+
+    return sval;
+}
+
+function getClickHandler() {
+    "use strict";
+
+    return function (info, tab) {
+
+        var authenticationTabId  = null,
+            imageSourceUrl       = info.srcUrl,
+            imageUploadHelperUrl = 'upload.html#',
+            vkCLientId           = '3315996',
+            vkRequestedScopes    = 'docs,offline',
+            vkAuthenticationUrl  = 'https://oauth.vk.com/authorize?client_id=' + vkCLientId + '&scope=' + vkRequestedScopes + '&redirect_uri=http%3A%2F%2Foauth.vk.com%2Fblank.html&display=page&response_type=token',
+            vkAccessToken,
+            vkAccessTokenExpiredFlag;
+
+        chrome.storage.local.get({'vkaccess_token': {}}, function (items) {
+
+            if (items.vkaccess_token.length > 0) {
+                imageUploadHelperUrl += imageSourceUrl + '&' + items.vkaccess_token;
+
+                chrome.tabs.create({url: imageUploadHelperUrl, selected: true});
+
+            } else {
+                chrome.tabs.create({url: vkAuthenticationUrl, selected: true}, function (tab) {
+                    authenticationTabId = tab.id;
+
+                    chrome.tabs.onUpdated.addListener(function tabUpdateListener(tabId, changeInfo) {
+
+                        if (tabId === authenticationTabId && changeInfo.url !== undefined && changeInfo.status === "loading") {
+
+                            if (changeInfo.url.indexOf('oauth.vk.com/blank.html') > -1) {
+                                authenticationTabId = null;
+                                chrome.tabs.onUpdated.removeListener(tabUpdateListener);
+
+                                vkAccessToken = getUrlParam(changeInfo.url, 'access_token');
+
+                                if (vkAccessToken !== undefined && vkAccessToken.length > 0) {
+
+                                    vkAccessTokenExpiredFlag = Number(getUrlParam(changeInfo.url, 'expires_in'));
+
+                                    if (vkAccessTokenExpiredFlag === 0) {
+                                        chrome.storage.local.set({'vkaccess_token': vkAccessToken}, function () {
+                                            chrome.tabs.update(
+                                                tabId,
+                                                {
+                                                    'url'   : 'upload.html#' + imageSourceUrl + '&' + vkAccessToken,
+                                                    'active': true
+                                                },
+                                                function (tab) {}
+                                            );
+                                        });
+                                    } else {
+                                        thereIsAnError('vk auth response problem', 'vkAccessTokenExpiredFlag != 0' + vkAccessToken);
+                                    }
+                                } else {
+                                    thereIsAnError('vk auth response problem', 'access_token length = 0 or vkAccessToken == undefined');
+                                }
+                            }
+                        }
+                    });
+                });
+            }
+        });
+    };
+}
+
+chrome.contextMenus.create({
+    "title": "Rehost on vk.com",
+    "type": "normal",
+    "contexts": ["image"],
+    "onclick": getClickHandler()
 });
 
-function thereIsAnError(textToShow, errorToShow)
-{
-  alert(textToShow + '\n' + errorToShow);
-}
